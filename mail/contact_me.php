664a8e5f09d576bb7291af31c177684fdf2cd4ef
Darran Blackweir <darran.blacky@gmail.com>
2018-09-21 02:26:42 +0100

    Set up heroku

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,23 +1,18 @@
 <?php
  
 
-if(isset($_POST['n'])){
-  $name = $_POST['n'];
+ if(isset($_POST['submit'])){
+  $name = $_POST['name'];
   $subject = $_POST['subject'];
-  $emailFrom = $_POST['e'];
-  $message = $_POST['m'];
-  $mailTo = "info@robbiemalone.com";
+  $emailFrom = $_POST['email'];
+  $message = $_POST['message'];
+  $mailTo = "darran.blacky@gmail.com";
   $header = "From: ".$emailFrom;
   $txt="You have recieved and email from ".$name.".\n\n".$message;
-  
-  $send = mail($mailTo, $subject, $body, $headers);
-
-  if($send){
-    echo '<br>';
-    echo 'Thanks for contacting me. I will be with you shortly';
-  } else {
-    echo 'Error: mjknejikntfjikr ';
-  }
-  
+  mail($mailTo, $subject, $txt, $header);
+  header("Location: index.php?mailsend");
 }
+if(!mail($mailTo, $subject, $txt, $header))
+  echo 'failed : dsnskfjdnc';
+
 ?>
