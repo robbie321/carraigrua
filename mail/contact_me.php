5b0288d1b77369d1779ad8111bc653056753202b
Darran Blackweir <darran.blacky@gmail.com>
2018-09-22 01:12:16 +0100

    emails now working

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -12,12 +12,12 @@
  } 
  
  
-  // $name = $_POST['name'];
-  // $subject = $_POST['subject'];
+  $name = $_POST['name'];
+  $subject = $_POST['subject'];
   // $from = $_POST['email'];
-  // $message = $_POST['message'];
+  $message = $_POST['message'];
   // $to = "info@robbiemalone.com";
-  // $header = "From: ".$emailFrom;
+  $header = "From: ".$emailFrom;
   // $content="You have recieved and email from ".$name.".\n\n".$message;
 
 
@@ -27,13 +27,13 @@
   // mail($to, $subject, $content, $header);
   // header("Location: index.php?mailsend");
 
-  $from = new SendGrid\Email(null, "test@example.com");
-  $subject = "Hello World from the SendGrid PHP Library!";
+  $from = new SendGrid\Email(null, $_POST['email']);
+  // $subject = "Hello World from the SendGrid PHP Library!";
   $to = new SendGrid\Email(null, "darran.blacky@gmail.com");
-  $content = new SendGrid\Content("text/plain", "Hello, Email!");
+  $content = new SendGrid\Content("You have recieved and email from ".$name.".\n\n".$message);
   $mail = new SendGrid\Mail($from, $subject, $to, $content);
   
-  $sg = new \SendGrid('KEY_GOES_HERE');
+  $sg = new \SendGrid('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');
   
   $response = $sg->client->mail()->send()->post($mail);
   echo $response->statusCode();
