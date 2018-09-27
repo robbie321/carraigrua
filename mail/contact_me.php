f8f86a425e6582a65220fc2e54f8ca2756b26157
Robbie Malone <robmalone7@gmail.com>
2018-09-27 20:21:51 +0100

    set up email api

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -17,7 +17,7 @@
   $fromMail = $_GET['email'];
   $message = $_GET['message'];
   $from = new SendGrid\Email(null, $fromMail);
-  $to = new SendGrid\Email(null, "robmalone7@gmail.com");
+  $to = new SendGrid\Email(null, "darran.blacky@gmail.com");
   $content = new SendGrid\Content("text/plain", $message);
   $mail = new SendGrid\Mail($from, $subject, $to, $content);
   
