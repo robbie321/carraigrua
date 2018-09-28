4c33bf1537dee140d74e58b146f154a693a110f7
Robbie Malone <robmalone7@gmail.com>
2018-09-28 15:11:59 +0100

    commit

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -17,18 +17,19 @@
   $fromMail = $_GET['email'];
   $message = $_GET['message'];
   $from = new SendGrid\Email(null, $fromMail);
-  $to = new SendGrid\Email(null, "carraigruabandb@eircom.net");
+//   $to = new SendGrid\Email(null, "carraigruabandb@eircom.net");
+  $to = new SendGrid\Email(null, "robmalone7@gmail.com");
   $content = new SendGrid\Content("text/plain", $message);
   $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
-  
-  $sg = new \SendGrid("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");
+  $sg = new \SendGrid('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');
+//   $sg = new \SendGrid("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");
 
   $response = $sg->client->mail()->send()->post($mail);
   echo $response->statusCode();
   echo $response->headers();
   echo $response->body();
 
-  header('Location:http://carraigruabandb.com/contact.html');
+//   header('Location:http://carraigruabandb.com/contact.html');
 
 ?>
