c658a982a847635ca92a171d70e9f2d28e091ee6
Robbie Malone <robmalone7@gmail.com>
2018-09-28 10:24:31 +0100

    email address change

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -17,7 +17,7 @@
   $fromMail = $_GET['email'];
   $message = $_GET['message'];
   $from = new SendGrid\Email(null, $fromMail);
-  $to = new SendGrid\Email(null, "carraigrua@eircom.net");
+  $to = new SendGrid\Email(null, "carraigruabandb@eircom.net");
   $content = new SendGrid\Content("text/plain", $message);
   $mail = new SendGrid\Mail($from, $subject, $to, $content);
   
