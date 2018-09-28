7567ab24c85d743154b7514727ed419f80b44632
Robbie Malone <robmalone7@gmail.com>
2018-09-28 11:25:57 +0100

    keywords added

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -21,7 +21,7 @@
   $content = new SendGrid\Content("text/plain", $message);
   $mail = new SendGrid\Mail($from, $subject, $to, $content);
   
-
+  $sg = new \SendGrid("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");
 
 
   $response = $sg->client->mail()->send()->post($mail);
